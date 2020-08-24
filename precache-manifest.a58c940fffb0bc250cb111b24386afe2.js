46a98a948982b8ad28d843b3bdd6ea6f9cebf6ac
Blacklotus0310 <bloodlotus0310@gmail.com>
2020-08-24 12:58:33 +0700

    Updates

---
+++ precache-manifest.a58c940fffb0bc250cb111b24386afe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb7d7d4368aad16b7c16c639fc34498",
+    "url": "/Lana/index.html"
+  },
+  {
+    "revision": "97ecbc87454e5ddb952a",
+    "url": "/Lana/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "430e6480170e1326d092",
+    "url": "/Lana/static/js/2.9e16c280.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lana/static/js/2.9e16c280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ecbc87454e5ddb952a",
+    "url": "/Lana/static/js/main.ae7dfb01.chunk.js"
+  },
+  {
+    "revision": "240191ef5c8fbac5ffbc",
+    "url": "/Lana/static/js/runtime-main.831f748e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Lana/static/media/logo.5d5d9eef.svg"
+  }
+]);
